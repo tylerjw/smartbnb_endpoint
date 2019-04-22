5908fd689ff4529a840e807345b6e522c531983f
Tyler Weaver <tylerjw@gmail.com>
2019-04-21 19:08:41 -0600

    Switching to using SQLAlchemy (#6)

--- server.py
+++ server.py
@@ -1,99 +1,131 @@
-from flask import Flask, request
 import json
-import datetime, time
+import datetime
+import time
 import os
-import dataset
 import re
 
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/smartbnb',methods=['POST'])
+## database objects ################################################
+
+class Reservation(db.Model):
+    # reservation table row
+    id = db.Column(db.String(10), primary_key=True)
+    phone = db.Column(db.String(20))
+    name = db.Column(db.String(80))
+    code = db.Column(db.String(4))
+    status = db.Column(db.String(20))
+    listing = db.Column(db.String(10))
+    start_date = db.Column(db.DateTime())
+    end_date = db.Column(db.DateTime())
+    listing = db.Column(db.Integer())
+
+    # construct a Reservation json data
+    def __init__(self, data):
+        super().__init__()
+        self.id = data['code']
+        self.phone = data['guest']['phone']
+        self.name = data['guest']['first_name'] + ' ' + data['guest']['last_name']
+        self.code = re.sub('[^0-9]', '', data['guest']['phone'])[-4:]
+        self.start_date = datetime.datetime.strptime(data['start_date'], '%Y-%m-%d').date()
+        self.end_date = datetime.datetime.strptime(data['end_date'], '%Y-%m-%d').date()
+        self.status = data['status']
+        self.listing = data['listing']['id']
+
+    def to_dict(self):
+        return dict(id=self.id,
+                    phone=self.phone,
+                    name=self.name,
+                    code=self.code,
+                    start_date=self.start_date.strftime('%Y-%m-%d'),
+                    end_date=self.end_date.strftime('%Y-%m-%d'),
+                    status=self.status,
+                    listing=self.listing)
+
+## flask routes ################################################
+@app.route('/smartbnb', methods=['POST'])
 def smarbnb():
-  data = json.loads(request.data.decode(), parse_float=float)
-  db = dataset.connect('sqlite:///database.db')
-  table = db.get_table('reservations', 
-    primary_id='id', primary_type=db.types.string(10))
+    # parse the json payload
+    data = json.loads(request.data.decode(), parse_float=float)
 
-  # write the file for later processing
-  timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
-  with open('data/{}.json'.format(timestamp), 'w') as outfile:
-    json.dump(data, outfile)
+    # write the file for later processing
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
+    with open('data/{}.json'.format(timestamp), 'w') as outfile:
+        json.dump(data, outfile)
 
-  # put the new data in the database
-  upsert(table, data)
+    # update the database with this new data
+    upsert(data)
 
-  #prune old values from database
-  today = datetime.date.today().strftime("%Y-%m-%d")
-  results = table.find(end_date={"<":today})
-  for row_id in [row['id'] for row in results]:
-    table.delete(id=row_id)
+    #prune old values from database
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    results = Reservation.query.filter(Reservation.end_date < today).all()
+    for row in results:
+        db.session.delete(row)
+        db.session.commit()
 
-  #let the caller know this worked
-  return "OK"
+    #let the caller know this worked
+    return "OK"
 
-@app.route('/listing/<listing_id>',methods=['GET'])
+@app.route('/listing/<listing_id>', methods=['GET'])
 def listing(listing_id):
-  db = dataset.connect('sqlite:///database.db')
-  table = db.get_table('reservations', 
-    primary_id='id', primary_type=db.types.string(10))
-  results = table.find(listing=listing_id)
-  rows = [row for row in results]
-  return json.dumps(rows)
+    rows = Reservation.query.filter(Reservation.listing == listing_id)
+    return json.dumps([r.to_dict() for r in rows])
 
-@app.route('/today/<listing_id>',methods=['GET'])
+@app.route('/today/<listing_id>', methods=['GET'])
 def today(listing_id):
-  db = dataset.connect('sqlite:///database.db')
-  table = db.get_table('reservations', 
-    primary_id='id', primary_type=db.types.string(10))
-  today = datetime.date.today().strftime("%Y-%m-%d")
-  results = table.find(
-    listing=listing_id, 
-    start_date={"<=":today},
-    end_date={">=":today})
-  rows = [row for row in results]
-  return json.dumps(rows)
+    today_date = datetime.date.today()
+    rows = Reservation.query.filter(
+        Reservation.listing == listing_id,
+        Reservation.start_date <= today_date,
+        Reservation.end_date >= today_date)
+    return json.dumps([r.to_dict() for r in rows])
 
-def upsert(table, data):
-  row = {
-    'id': data['code'],
-    'phone': data['guest']['phone'],
-    'name': data['guest']['first_name'] + ' ' + data['guest']['last_name'],
-    'code': re.sub('[^0-9]','',data['guest']['phone'])[-4:],
-    'start_date': data['start_date'],
-    'end_date': data['end_date'],
-    'status': data['status'],
-    'listing': data['listing']['id'],
-    'updated': datetime.datetime.utcnow().ctime()
-  }
-  
-  if (row['status'] == 'cancelled'):
-    table.delete(id=row['id'])
-  else:
-    table.upsert(row, ['id'])
+## helper functions #############################################
+def upsert(data):
+    # create the new row object
+    new_row = Reservation(data)
+    # test to see if we should delete or add
+    if data['status'] == 'cancelled':
+        db.session.delete(new_row)
+    else:
+        existing = Reservation.query.get(new_row.id)
+        if existing:
+            # update
+            existing = new_row
+        else:
+            # insert
+            db.session.add(new_row)
+    #commit the change
+    db.session.commit()
 
 # loads the files into the database
 def load_files():
-  db = dataset.connect('sqlite:///database.db')
-  table = db.get_table('reservations', 
-    primary_id='id', primary_type=db.types.string(10))
+    # import the files in order of modification time
+    files = [os.path.join('data', f) for f in os.listdir('data')]
+    files.sort(key=os.path.getmtime)
 
-  # import the files in order of modification time
-  files = [os.path.join('data', f) for f in os.listdir('data')]
-  files.sort(key=lambda filename: os.path.getmtime(filename))
+    for filename in files:
+        with open(filename, 'r') as file:
+            data = json.loads(file.read())
+            upsert(data)
 
-  for filename in files:
-    with open(filename, 'r') as file:
-      data = json.loads(file.read())
-      upsert(table, data)
-      
-
+## main ############################################################
 if __name__ == '__main__':
-  # create data directory if it doesn't exist
-  if not os.path.exists('data'):
-    os.makedirs('data')
+    # create data directory if it doesn't exist
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
-  # load the files
-  load_files()
+    # create the database
+    db.create_all()
 
-  # start the server
-  app.run(host='0.0.0.0', port=9004)
+    # load the files
+    load_files()
+
+    # start the server
+    app.run(host='0.0.0.0', port=9004)
